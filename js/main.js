586fecd8df1d391c4cc821ceefee9d50c692eeba
marlaurindo <marlaurindo@gmail.com>
2020-06-02 08:10:08 -0300

    codigo javascript e html / css

--- js/main.js
+++ js/main.js
@@ -1,21 +1,71 @@
-/* Inicio funções calculadora */
+var displayBuffer = "";
+var numero = "";
+var termos = [undefined,undefined,undefined];
+var resultado = undefined;
 
-var q = 10;
-var w = 5;
-var conta = "+";
-
-function calculadora(q,w,conta){
-    if (conta === "+"){
-        return q + w;
-    }else if(conta === "-"){
-        return q - w;
-    }else if (conta === "*"){
-        return q * w;
-    }else if (conta === "/"){
-        return q / w;
-    }else{
-        document.write("<h1> Não foi possivel calcular .</h1>");
-    }
+function pressNum(num){
+    numero = numero.concat(num.innerHTML);
+    showDisplay(num.innerHTML);
 }
 
-document.write("<h1>" +calculadora(q,w,conta) + "</h1>")
+function pressOperator(op){
+    if(termos[1]==undefined){
+        termos[0] = numero;
+        termos[1] = (op.innerHTML);
+        showDisplay(op.innerHTML);
+        numero = "";
+    }
+    
+}
+
+function pressEqual(){
+    if(termos[0] != undefined && termos[1] != undefined && numero != ""){
+        termos[2] = numero;
+        var keepreresultado;
+        switch(termos[1]){
+            case '+':
+                resultado = Number(termos[0])+ Number(termos[2]);
+                break;
+            case '-':
+                resultado = Number(termos[0])- Number(termos[2]);
+                break;
+            case '*':
+                resultado = Number(termos[0])* Number(termos[2]);
+                break;
+            case '/':
+                resultado = Number(termos[0])/ Number(termos[2]);
+                break;
+                
+                 
+        }
+        keepreresultado=resultado;
+        clearDisplay();
+        showDisplay(resultado);
+        clearMemory();
+        numero = keepreresultado;
+    }
+    
+}
+
+function clearMemory(){
+    numero = "";
+    termos = [undefined,undefined,undefined];
+    resultado = undefined;
+}
+
+function clearDisplay(){
+    displayBuffer = "";
+    var tela = document.getElementById('display');
+    tela.value = displayBuffer;
+}
+
+function clearAll(){
+    clearDisplay();
+    clearMemory();
+}
+
+function showDisplay(conteudo){
+    displayBuffer = displayBuffer.concat(conteudo);
+    var tela = document.getElementById('display'); 
+    tela.value = displayBuffer;
+}
